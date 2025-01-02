d59303df133fa83fcc841e438f47ded4f58fbf70
minorele <658366702@xiaomi.com>
2025-01-02 19:58:50 +0800

    新增：c端用户界面分类菜品展示功能

---
+++ sky-server/src/main/java/org/cheems/controller/user/DishController.java
@@ -0,0 +1,45 @@
+package org.cheems.controller.user;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.cheems.constant.StatusConstant;
+import org.cheems.entity.Dish;
+import org.cheems.result.Result;
+import org.cheems.service.DishService;
+import org.cheems.vo.DishVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController("userDishController")
+@RequestMapping("/user/dish")
+@Slf4j
+@Api(tags = "C端-菜品浏览接口")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+
+    /**
+     * 根据分类id查询菜品
+     *
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询菜品")
+    public Result<List<DishVO>> list(Long categoryId) {
+        log.info("用户正在查询的分类id下的菜品，分类id是：{}：",categoryId);
+        Dish dish = new Dish();
+        dish.setCategoryId(categoryId);
+        dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
+
+        List<DishVO> list = dishService.listWithFlavor(dish);
+
+        return Result.success(list);
+    }
+
+}
